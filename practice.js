57870c746cfb01b5ae9a5e754a9a8c81c1bb70cb
Ediful <56943939+Ediful@users.noreply.github.com>
2022-01-26 23:06:57 -0600

    feat: Caesar Cipher and Analyze Array tests

--- practice.js
+++ practice.js
@@ -6,4 +6,54 @@ function reverseString(string) {
   return string.split('').reverse().join('');
 }
 
-export { capitalizeString, reverseString };
+const calculator = (() => {
+  const add = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+  const mul = (a, b) => a * b;
+  const div = (a, b) => a / b;
+  return {
+    add,
+    sub,
+    mul,
+    div,
+  };
+})();
+
+function caesarCipher(string, shift) {
+  let encryptedString = '';
+  for (let i = 0; i < string.length; i += 1) {
+    let charCode = string.charCodeAt(i);
+    if (charCode < 64) {
+      charCode += 0;
+    } else if (charCode === 90 || charCode === 122) {
+      charCode -= 25;
+    } else charCode += shift;
+    encryptedString += String.fromCharCode(charCode);
+  }
+
+  return encryptedString;
+}
+
+function analyzeArray(array) {
+  const object = {
+    average: null,
+    min: null,
+    max: null,
+    length: null,
+  };
+
+  object.average = array.reduce((a, b) => a + b, 0) / array.length;
+  object.min = Math.min(...array);
+  object.max = Math.max(...array);
+  object.length = array.length;
+
+  return object;
+}
+
+export {
+  capitalizeString,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+};
--- practice.test.js
+++ practice.test.js
@@ -1,4 +1,10 @@
-import { capitalizeString, reverseString } from './practice';
+import {
+  capitalizeString,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from './practice';
 
 test('Capitalize string', () => {
   expect(capitalizeString('apple')).toBe('Apple');
@@ -6,4 +12,34 @@ test('Capitalize string', () => {
 
 test('Reverse string', () => {
   expect(reverseString('apple')).toBe('elppa');
+});
+
+test('Calculator Addition', () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+test('Calculator Subtraction', () => {
+  expect(calculator.sub(2, 1)).toBe(1);
+});
+
+test('Calculator Multiplication', () => {
+  expect(calculator.mul(2, 3)).toBe(6);
+});
+
+test('Calculator Division', () => {
+  expect(calculator.div(6, 2)).toBe(3);
+});
+
+test('Caesar Cipher', () => {
+  expect(caesarCipher('Czar!', 1)).toBe('Dabs!');
+});
+
+test('Analyze Array', () => {
+  const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+  expect(object).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
